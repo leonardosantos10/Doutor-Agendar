1450aaa376992a286f8b7c2fc030393727ddbc21
leonardosantos10 <leob35230@gmail.com>
2026-01-04 23:50:24 -0300

    adjusting the dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,16 @@
-const DashboardPage = () => {
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+
+const DashboardPage =  () => {
+const session = authClient.useSession()
     return (
    <div>
       <h1>Dashboard</h1>
+      <h1>{session?.data?.user?.name}</h1>
+      <h1>{session?.data?.user?.email}</h1>
+      <Button onClick={() => authClient.signOut()}>Sair</Button>
    </div>
 );
 };

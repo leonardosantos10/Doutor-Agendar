566110aa8b66900fd2345d0fff8c1d1952ab7fdc
leonardosantos10 <leob35230@gmail.com>
2026-01-11 08:33:25 -0300

    feat: add sign in with google

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -16,6 +16,7 @@ if (!session?.user) {
    redirect("/authentication");
 }
 
+
 const clinics = await db.query.usersToClinicsTable.findMany({
   where: eq(usersToClinicsTable.userId, session.user.id),
 })

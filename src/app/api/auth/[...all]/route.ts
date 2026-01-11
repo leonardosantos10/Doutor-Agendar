4801076381f1b2cd6a2558d2e354702d6aa71078
leonardosantos10 <leob35230@gmail.com>
2026-01-10 22:32:16 -0300

    feat: add clinic form page

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -3,3 +3,5 @@ import { toNextJsHandler } from "better-auth/next-js";
 import { auth } from "@/lib/auth"; // path to your auth file
 
 export const { POST, GET } = toNextJsHandler(auth);
+
+
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,33 @@
-"use client";
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
+import { auth} from "@/lib/auth";
 
-const DashboardPage =  () => {
-const session = authClient.useSession()
+import SignOutButton from "./components/sign-out-button";
+
+const DashboardPage = async  () => {
+const session = await auth.api.getSession({ 
+    headers: await headers(),
+});
+if (!session?.user) {
+   redirect("/authentication");
+}
+
+const clinics = await db.query.usersToClinicsTable.findMany({
+  where: eq(usersToClinicsTable.userId, session.user.id),
+})
+if (clinics.length === 0) {
+  redirect("/clinic-form");
+}
     return (
    <div>
       <h1>Dashboard</h1>
-      <h1>{session?.data?.user?.name}</h1>
-      <h1>{session?.data?.user?.email}</h1>
-      <Button onClick={() => authClient.signOut()}>Sair</Button>
+      <h1>{session?.user?.name}</h1>
+      <h1>{session?.user?.email}</h1>
+      <SignOutButton />
    </div>
 );
 };
